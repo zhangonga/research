1fd33f97a6c37b20da966907b829002f5aa4e8b5
zhanggong <zhanggong@58.com>
2020-07-10 14:12:19 +0800

    Int

--- jdk/src/main/java/tech/zg/research/jdk/integer/Main.java
+++ jdk/src/main/java/tech/zg/research/jdk/integer/Main.java
@@ -12,13 +12,17 @@ public class Main {
     public static void main(String[] args) {
 
         for (int index = 1; index < 100; index++) {
-            int i = Integer.numberOfLeadingZeros(index);
-            int i1 = Integer.numberOfTrailingZeros(index);
-            int i2 = roundToPowerOfTwo(index);
+//            int i = Integer.numberOfLeadingZeros(index);
+//            int i1 = Integer.numberOfTrailingZeros(index);
+//            int i2 = roundToPowerOfTwo(index);
+//            out.println(i);
+//            out.println(i1);
+//            out.println(i2);
+            int i = numberOfLeadingZeros(index);
             out.println(i);
-            out.println(i1);
-            out.println(i2);
         }
+
+
     }
 
     /**
@@ -36,5 +40,32 @@ public class Main {
         }
         final int nextPow2 = 1 << (32 - Integer.numberOfLeadingZeros(value - 1));
         return nextPow2;
+    }
+
+
+    public static int numberOfLeadingZeros(int i) {
+        // HD, Figure 5-6
+        if (i == 0) {
+            return 32;
+        }
+        int n = 1;
+        if (i >>> 16 == 0) {
+            n += 16;
+            i <<= 16;
+        }
+        if (i >>> 24 == 0) {
+            n += 8;
+            i <<= 8;
+        }
+        if (i >>> 28 == 0) {
+            n += 4;
+            i <<= 4;
+        }
+        if (i >>> 30 == 0) {
+            n += 2;
+            i <<= 2;
+        }
+        n -= i >>> 31;
+        return n;
     }
 }
